c95a8057a75923e2a7828d831fcd6970e8b61fb4
Baronne_Noire <party2015ru@mail.ru>
2025-03-28 10:51:32 +0500

    add house scripts

---
+++ Assets/_Game/Develop/Scripts/Gameplay/Features/Houses/Army/KnightHouseService.cs
@@ -0,0 +1,23 @@
+using Assets.Gameplay.Building;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightHouseService : HouseBase
+{
+    public KnightHouseService(BuildingConfig buildingConfig, int level) : base(buildingConfig, level)
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_Game/Develop/Scripts/Gameplay/Features/Houses/Support/TavernService.cs
@@ -0,0 +1,23 @@
+using Assets.Gameplay.Building;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TavernService : HouseBase
+{
+    public TavernService(BuildingConfig buildingConfig, int level) : base(buildingConfig, level)
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
